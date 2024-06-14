c32444d34bd2e185757bd083084865cf58e0b6c3
deryantiamaliah <dery23039si@student.nurulfikri.ac.id>
2024-06-14 13:05:26 +0700

    pertemuan 13

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,40 @@
 <?php
 
 use App\Http\Controllers\MovieController;
-use App\Http\Controllers\GenreController;
-use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', function () {
-    return view('product');
+Route::resource('/movies', MovieController::class);
+
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/movies', [MovieController::class, 'index']);
+    Route::get('/genres', [GenreController::class, 'index']);
+    Route::get('/reviews', [ReviewController::class, 'index']);
 });
 
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/create', [MovieController::class, 'create']);
-Route::post('/movies', [MovieController::class, 'store']);
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
-
-
-Route::get('/genres', [GenreController::class,'index']);
-Route::get('/genres/create', [GenreController::class, 'create']);
-Route::post('/genres', [GenreController::class, 'store']);
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
-
-
-Route::get('/reviews', [ReviewController::class, 'index']);
-Route::get('/reviews/create', [ReviewController::class, 'create']);
-Route::post('/reviews', [ReviewController::class, 'store']);
-Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
+require __DIR__.'/auth.php';
